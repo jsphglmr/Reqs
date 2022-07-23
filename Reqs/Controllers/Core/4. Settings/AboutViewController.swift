060db57e201bf6db861ce0a0091fca3d347ff8ff
Jojo <jsphglmr@gmail.com>
2022-07-23 00:03:48 -0700

    setup about settings vc

--- Reqs/Controllers/Core/4. Settings/AboutViewController.swift
+++ Reqs/Controllers/Core/4. Settings/AboutViewController.swift
@@ -15,11 +15,14 @@ class AboutViewController: UIViewController {
 
 extension AboutViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
+        return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "About Placeholder"
+        cell.selectionStyle = .none
+        return cell
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
