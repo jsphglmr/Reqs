fd14c3bd524896ec43e9bd7616e9172afab9dce6
Jojo <jsphglmr@gmail.com>
2022-07-23 00:05:25 -0700

    setup location settings tableview

--- Reqs/Controllers/Core/4. Settings/LocationSettingsViewController.swift
+++ Reqs/Controllers/Core/4. Settings/LocationSettingsViewController.swift
@@ -15,11 +15,14 @@ class LocationSettingsViewController: UIViewController {
 
 extension LocationSettingsViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
+        return 5
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Location settings text placeholder"
+        cell.selectionStyle = .none
+        return cell
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
