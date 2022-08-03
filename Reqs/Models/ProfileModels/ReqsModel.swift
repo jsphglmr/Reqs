463e3c5cceca70c86fa91e11e2fce981898b32ba
Jojo <jsphglmr@gmail.com>
2022-08-03 13:52:24 -0700

    req model configuration for Realm

--- Reqs/Models/ProfileModels/ReqsModel.swift
+++ Reqs/Models/ProfileModels/ReqsModel.swift
@@ -10,8 +10,29 @@ import RealmSwift
 
 class ReqsModel: Object {
     
-    @Persisted var title: String = ""
     @Persisted var dateCreated: Date?
+    
+    @Persisted var name: String
+    @Persisted var url: String
+    @Persisted var imageUrl: String
+    @Persisted var rating: Double?
+    @Persisted var price: String?
+    @Persisted var phone: String?
+    
+    @Persisted var city: String
+    @Persisted var country: String
+    @Persisted var state: String
+    @Persisted var address1: String?
+    @Persisted var address2: String?
+    @Persisted var address3: String?
+    @Persisted var zipCode: String?
+    
+    
+    @Persisted var latitude: Double
+    @Persisted var longitude: Double
+    
+    @Persisted var alias: String?
+    
     
     @Persisted(originProperty: "items") var parentCategory: LinkingObjects<ProfileModel>
 }
