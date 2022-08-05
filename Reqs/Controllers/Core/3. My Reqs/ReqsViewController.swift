cf9caa98e4c83cb91d20fe97b2ee2941acc1b9cf
Jojo <jsphglmr@gmail.com>
2022-08-04 20:05:39 -0700

    refactor old vc to single vc. reqsvc does not present new vc anymore

--- Reqs/Controllers/Core/3. My Reqs/ReqsViewController.swift
+++ Reqs/Controllers/Core/3. My Reqs/ReqsViewController.swift
@@ -5,4 +5,83 @@
 //  Created by Joseph Gilmore on 8/4/22.
 //
 
-import Foundation
+import UIKit
+import RealmSwift
+
+//MARK: - UIViewController
+class ReqsViewController: UIViewController {
+    
+    lazy var realm: Realm = {
+        return try! Realm()
+    }()
+    
+    var reqsList: Results<ReqsModel>? {
+        didSet {
+            profileTableView.reloadData()
+        }
+    }
+    var business: Business?
+    
+    private let profileTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupNavigation()
+        loadReqs()
+        
+        profileTableView.delegate = self
+        profileTableView.dataSource = self
+    }
+    
+    private func loadReqs() {
+        reqsList = realm.objects(ReqsModel.self)
+        view.addSubview(profileTableView)
+        profileTableView.frame = view.bounds
+        profileTableView.reloadData()
+    }
+    
+    private func setupNavigation() {
+        navigationItem.title = "Reqs List"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.barTintColor = .systemBackground
+        navigationItem.rightBarButtonItem?.tintColor = .label
+        navigationController?.navigationBar.isTranslucent = false
+    }
+}
+
+//MARK: - Tableview Delegate & Datasource
+extension ReqsViewController: UITableViewDelegate, UITableViewDataSource {
+    //datasource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return reqsList?.count ?? 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let selectedProfile = reqsList?[indexPath.row]
+        cell.textLabel?.text = selectedProfile?.name.capitalized
+        return cell
+    }
+    
+    //delegate
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        let annotationVC = AnnotationViewController(business: indexPath.row)
+//        navigationController?.pushViewController(annotationVC, animated: true)
+    }
+
+// TODO: Add Delete from Realm CRUD op
+//    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+//        if editingStyle == .delete {
+//            tableView.deleteRows(at: [indexPath], with: .fade)
+//            //TODO: Remove from Realm
+//        }
+//    }
+
+
+}
