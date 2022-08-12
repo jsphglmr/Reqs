80c0b369c533b3a920ad47001bc3ad4a43232b0f
Jojo <jsphglmr@gmail.com>
2022-08-12 11:35:00 -0700

    remove old tableview VCs, add custom settings and settings switch cells

---
+++ Reqs/Controllers/Cells/SettingsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SettingsTableViewCell.swift
+//  Reqs
+//
+//  Created by Joseph Gilmore on 8/12/22.
+//
+
+import UIKit
+
+class SettingsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Reqs/Controllers/Cells/SwitchTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SwitchTableViewCell.swift
+//  Reqs
+//
+//  Created by Joseph Gilmore on 8/12/22.
+//
+
+import UIKit
+
+class SwitchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
