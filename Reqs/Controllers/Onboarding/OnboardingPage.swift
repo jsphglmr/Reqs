50b0e7042af1fc4281fc994751add38554048248
Jojo <jsphglmr@gmail.com>
2022-08-22 20:31:48 -0700

    format onboarding images

--- Reqs/Controllers/Onboarding/OnboardingPage.swift
+++ Reqs/Controllers/Onboarding/OnboardingPage.swift
@@ -9,7 +9,6 @@ import UIKit
 
 class OnboardingPage: UIViewController {
     
-    let imageView = UIImageView()
     let titleLabel = UILabel()
     let subtitleLabel = UILabel()
     var setBackgroundColor = UIColor()
@@ -20,8 +19,18 @@ class OnboardingPage: UIViewController {
         stackView.axis = .vertical
         stackView.alignment = .center
         stackView.distribution = .fill
-        stackView.spacing = 10
+        stackView.spacing = 20
         return stackView
+    }()
+    
+    private lazy var onboardingImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.layer.cornerRadius = 10
+        imageView.layer.masksToBounds = true
+        imageView.clipsToBounds = true
+        return imageView
     }()
     
     private lazy var mainTitle: UILabel = {
@@ -44,7 +53,7 @@ class OnboardingPage: UIViewController {
 
     init(imageName: String, titleText: String, subtitleText: String, backgroundColor: UIColor) {
         super.init(nibName: nil, bundle: nil)
-        imageView.image = UIImage(named: imageName)
+        onboardingImage.image = UIImage(named: imageName)
         mainTitle.text = titleText
         mainSubtitle.text = subtitleText
         setBackgroundColor = backgroundColor
@@ -62,9 +71,12 @@ class OnboardingPage: UIViewController {
     
     
     func layout() {
+        
         view.addSubview(stackView)
+        stackView.addArrangedSubview(onboardingImage)
         stackView.addArrangedSubview(mainTitle)
         stackView.addArrangedSubview(mainSubtitle)
+        
         
         view.backgroundColor = setBackgroundColor
         
@@ -73,6 +85,11 @@ class OnboardingPage: UIViewController {
             stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ]
         
+        let onboardingImageConstraints = [
+            onboardingImage.heightAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5)
+        ]
+        
+        NSLayoutConstraint.activate(onboardingImageConstraints)
         NSLayoutConstraint.activate(stackViewConstraints)
     }
 }
