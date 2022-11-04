f2335a9750cf74e52bb951bd5461127da976bfb9
Jojo <jsphglmr@gmail.com>
2022-11-03 22:52:37 -0700

    fix navigation help button

--- Reqs/Shared/Utilities/UIViewController + Utilities.swift
+++ Reqs/Shared/Utilities/UIViewController + Utilities.swift
@@ -10,9 +10,13 @@ import UIKit
 extension UIViewController {
     func addNavigationHelpItem() {
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "info.circle"), primaryAction: UIAction { _ in
-            let alert = UIAlertController(title: "Info", message: "Body Info", preferredStyle: .alert)
+            let alert = UIAlertController(title: "Getting Started", message: "Use the map to search for places near you. Once you find a place you love or want to try, save it to your list!", preferredStyle: .alert)
             let cancel = UIAlertAction(title: "Cancel", style: .cancel)
-            let mapButton = UIAlertAction(title: "Go to map!", style: .default)
+            let mapButton = UIAlertAction(title: "Go to map!", style: .default, handler: { _ in
+                //move to index 0 of tabbar once tapped
+                self.tabBarController?.selectedIndex = 0
+            })
+            
             alert.addAction(cancel)
             alert.addAction(mapButton)
             self.present(alert, animated: true)
