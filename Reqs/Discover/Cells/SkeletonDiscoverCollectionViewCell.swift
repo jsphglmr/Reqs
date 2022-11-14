f82349b0aae8aa093ca1aa4405996ff466cf05ba
Jojo <jsphglmr@gmail.com>
2022-11-13 21:10:59 -0800

    prepare skeleton cell loaders. add skeleton cell and skeleton loadable files

--- Reqs/Discover/Cells/SkeletonDiscoverCollectionViewCell.swift
+++ Reqs/Discover/Cells/SkeletonDiscoverCollectionViewCell.swift
@@ -5,4 +5,35 @@
 //  Created by Joseph Gilmore on 11/13/22.
 //
 
-import Foundation
+import UIKit
+
+extension SkeletonDiscoverCollectionViewCell: SkeletonLoadable {}
+
+class SkeletonDiscoverCollectionViewCell: UICollectionViewCell {
+    static let identifier = "SkeletonDiscoverCollectionViewCell"
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(imageView)
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        imageView.frame = contentView.bounds
+    }
+}
