1b9588303e690f8c8d1b44e5e2c34897de98a6d9
Jojo <jsphglmr@gmail.com>
2022-11-13 21:04:37 -0800

    refactor files

---
+++ Reqs/Discover/Cells/SkeletonDiscoverCollectionViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  SkeletonDiscoverCollectionViewCell.swift
+//  Reqs
+//
+//  Created by Joseph Gilmore on 11/13/22.
+//
+
+import Foundation
---
+++ Reqs/Shared/Utilities/SkeletonLoadable.swift
@@ -0,0 +1,8 @@
+//
+//  SkeletonLoadable.swift
+//  Reqs
+//
+//  Created by Joseph Gilmore on 11/13/22.
+//
+
+import Foundation
